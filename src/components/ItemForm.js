56d2ae98bbd4ee52766dad106963f1ee4da28cc9
Krithik Singh <krithiksingh@gmail.com>
2023-12-29 14:44:32 -0500

    lab completed

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,52 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
+function ItemForm({ onItemFormSubmit }) {
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('Produce');
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
 
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+    function handleCategoryChange(event) {
+        setCategory(event.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        onItemFormSubmit({
+            id: uuid(),
+            name,
+            category,
+        });
+    }
+    return (
+        <form onSubmit={handleSubmit} className='NewItem'>
+            <label>
+                Name:
+                <input
+                    onChange={handleNameChange}
+                    type='text'
+                    name='name'
+                    value={name}
+                />
+            </label>
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
+            <label>
+                Category:
+                <select
+                    name='category'
+                    onChange={handleCategoryChange}
+                    value={category}
+                >
+                    <option value='Produce'>Produce</option>
+                    <option value='Dairy'>Dairy</option>
+                    <option value='Dessert'>Dessert</option>
+                </select>
+            </label>
+
+            <button type='submit'>Add to List</button>
+        </form>
+    );
 }
 
 export default ItemForm;
